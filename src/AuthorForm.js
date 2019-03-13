0c7a47ea3157fc6c3e2c42cc718ab5d7f01d46de
riri-jp <alahmary.mashari@gmail.com>
2019-03-13 21:26:29 +0300

    Forms

--- src/AuthorForm.js
+++ src/AuthorForm.js
@@ -11,6 +11,8 @@ class AuthorForm extends Component {
     books: []
   };
 
+  textChangeHandler = event =>
+    this.setState({ [event.target.name]: event.target.value });
   componentWillUnmount() {
     if (this.props.errors.length) this.props.resetErrors();
   }
@@ -37,19 +39,37 @@ class AuthorForm extends Component {
             <div className="input-group-prepend">
               <span className="input-group-text">First Name</span>
             </div>
-            <input type="text" className="form-control" name="first_name" />
+            <input
+              type="text"
+              className="form-control"
+              name="first_name"
+              value={this.state.first_name}
+              onChange={this.textChangeHandler}
+            />
           </div>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">Last Name</span>
             </div>
-            <input type="text" className="form-control" name="last_name" />
+            <input
+              type="text"
+              className="form-control"
+              name="last_name"
+              value={this.state.last_name}
+              onChange={this.textChangeHandler}
+            />
           </div>
           <div className="input-group mb-3">
             <div className="input-group-prepend">
               <span className="input-group-text">Image URL</span>
             </div>
-            <input type="text" className="form-control" name="imageUrl" />
+            <input
+              type="text"
+              className="form-control"
+              name="imageUrl"
+              value={this.state.imageUrl}
+              onChange={this.textChangeHandler}
+            />
           </div>
           <input type="submit" />
         </form>
--- src/BookForm.js
+++ src/BookForm.js
@@ -1,9 +1,84 @@
 import React, { Component } from "react";
 
+import * as actionCreators from "./store/actions";
+import { connect } from "react-redux";
+import { error } from "util";
+
 class BookForm extends Component {
+  state = {
+    title: "",
+    color: ""
+  };
+
+  handleChange = event =>
+    this.setState({ [event.target.name]: event.target.value });
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.postBook(this.state, this.props.author, this.props.closeModal);
+  };
+
   render() {
-    return <h1>I'M NOT EVEN A FORM YET!</h1>;
+    let colors = [
+      "red",
+      "blue",
+      "green",
+      "white",
+      "yellow",
+      "orange",
+      "black"
+    ].map(color => <option value={color}>{color}</option>);
+    return (
+      <form onSubmit={this.handleSubmit}>
+        {!!this.props.errors.length && (
+          <div className="alert alert-danger" role="alert">
+            {this.props.errors.map(error => (
+              <p key={error}>{error}</p>
+            ))}
+          </div>
+        )}
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">Title</span>
+          </div>
+          <input
+            placeholder="title"
+            name="title"
+            value={this.state.title}
+            onChange={this.handleChange}
+          />
+        </div>
+        <div className="input-group mb-3">
+          <div className="input-group-prepend">
+            <span className="input-group-text">Color</span>
+          </div>
+
+          <select name="color" onChange={this.handleChange}>
+            {colors}
+          </select>
+        </div>
+        <input type="submit" />
+      </form>
+    );
   }
 }
 
-export default BookForm;
+const mapStateToProps = state => {
+  return {
+    author: state.rootAuthor.author,
+    errors: state.rootErrors.errors
+  };
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    postBook: (newBook, author, closeModal) =>
+      dispatch(actionCreators.postAuthor(newBook, author, closeModal)),
+    resetErrors: () => dispatch(actionCreators.resetErrors())
+  };
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(BookForm);
